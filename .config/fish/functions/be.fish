758bade1e5714adc113196df5990401e92aa712c
Farzad Ghanei <farzad.ghanei@gmail.com>
2017-12-04 15:55:33 +0100

    Add fish functions: be.fish and workon.fish
    virtualenvwrapper for fish? ;p

---
+++ .config/fish/functions/be.fish
@@ -0,0 +1,19 @@
+function --description "work on a project" be
+    set -l name $argv[1]
+    set -l venv_path ~/.virtualenvs/$name
+    set -l work_path ~/projects/$name
+
+    if test -z $name
+        echo "no project name specified!"
+    else
+        if test -e $venv_path
+            workon $name
+        end
+
+        if test -e $work_path
+            cd $work_path
+        else
+            echo "project work dir not found on $work_path"
+        end
+    end
+end
