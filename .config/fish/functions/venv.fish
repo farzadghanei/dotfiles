45e40d3f60bf8eb4fa91cc3f0f448e497bd77203
Farzad Ghanei <farzad.ghanei@gmail.com>
2018-10-02 21:51:37 +0200

    Fix fish function description order
    Descrption should be after the function name

--- .config/fish/functions/venv.fish
+++ .config/fish/functions/venv.fish
@@ -1,4 +1,4 @@
-function --description "activate Python virtualenv from ~/.virtualenvs" venv
+function venv --description "activate Python virtualenv from ~/.virtualenvs"
     set -l venv_path ~/.virtualenvs
     set -l venv "$venv_path/$argv[1]/bin/activate.fish"
     source $venv
