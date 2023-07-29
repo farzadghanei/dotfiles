27fc5f37347c5d2b7e9e6d615c705365072c502e
Farzad Ghanei <644113+farzadghanei@users.noreply.github.com>
2023-07-29 11:05:43 +0200

    feat(fish): gitmm supports main branch as well as master

--- .config/fish/functions/gitmm.fish
+++ .config/fish/functions/gitmm.fish
@@ -1,5 +1,9 @@
 function gitmm --description "Git merge origin to local master, and prune"
+    set -l branch master
+    if ! git branch | grep -q $branch
+        set branch main
+    end
     git fetch --prune
-    git checkout master
-    git merge origin/master
+    git checkout $branch
+    git merge origin/$branch
 end
