0850d8ff81a487d032187d4dee18043987448de4
Farzad Ghanei <644113+farzadghanei@users.noreply.github.com>
2024-01-06 18:03:41 +0100

    chore(formatting): format python scripts and colortrans runs with py3

--- bin/config-vim.py
+++ bin/config-vim.py
@@ -25,16 +25,16 @@ import os
 import shutil
 import subprocess
 
-__version__   = '2.0.0'
+__version__ = "2.0.0"
 dotfiles = os.path.dirname(os.path.dirname(__file__))
 
 
 def ask_yes_no(msg, default_accept=True):
-    default = ' [Y/n] ' if default_accept else ' [y/N] '
+    default = " [Y/n] " if default_accept else " [y/N] "
     ans = input(msg + default).strip().lower()
     if default_accept:
-        return ans != 'n'
-    return ans == 'y'
+        return ans != "n"
+    return ans == "y"
 
 
 def configure_vim(target, ask=True):
@@ -45,24 +45,21 @@ def configure_vim(target, ask=True):
 
 
 def copy_dot_vim(src, dst, ask=True):
-    src_vimrc = os.path.join(src, '.vimrc')
-    dst_vimrc = os.path.join(dst, '.vimrc')
+    src_vimrc = os.path.join(src, ".vimrc")
+    dst_vimrc = os.path.join(dst, ".vimrc")
 
-    src_dot_vim = os.path.join(src, '.vim')
-    dst_dot_vim = os.path.join(dst, '.vim')
+    src_dot_vim = os.path.join(src, ".vim")
+    dst_dot_vim = os.path.join(dst, ".vim")
 
     if os.path.exists(dst_vimrc):
-        if ask and not ask_yes_no(
-                "Replace existing vimrc on '{}'?".format(dst_vimrc)):
+        if ask and not ask_yes_no("Replace existing vimrc on '{}'?".format(dst_vimrc)):
             raise IOError("Destination vimrc exists on '{}'".format(dst_vimrc))
         os.remove(dst_vimrc)
     shutil.copy(src_vimrc, dst_vimrc)
 
     if os.path.exists(dst_dot_vim):
-        if ask and not ask_yes_no(
-                "Replace existing .vim on '{}'?".format(dst_dot_vim)):
-            raise IOError(
-                "Destination .vim exists on '{}'".format(dst_dot_vim))
+        if ask and not ask_yes_no("Replace existing .vim on '{}'?".format(dst_dot_vim)):
+            raise IOError("Destination .vim exists on '{}'".format(dst_dot_vim))
         shutil.rmtree(dst_dot_vim)
     shutil.copytree(src_dot_vim, dst_dot_vim)
 
@@ -73,15 +70,15 @@ def install_plugins_with_plug(target, ask):
     https://github.com/junegunn/vim-plug
     """
     if ask and ask_yes_no("Install vim-plug plugins?"):
-        vimrc = os.path.join(target, '.vimrc')
-        subprocess.call(['vim', '-u', vimrc, '+PlugInstall'])
+        vimrc = os.path.join(target, ".vimrc")
+        subprocess.call(["vim", "-u", vimrc, "+PlugInstall"])
 
 
 def main():
-    home_path = os.path.expanduser('~')
+    home_path = os.path.expanduser("~")
     target = sys.argv[1] if len(sys.argv) > 1 else home_path
     return configure_vim(target) and 1 or 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
